230bc27e6c37aed7a4fec976d75e726c650cd75f
MichaelRzheutski <michael.rzheutski@gmail.com>
2024-02-06 15:46:42 +0300

    API tests for Product and Cart

---
+++ src/main/java/com/solvd/api_tests/api/GetRecipeById.java
@@ -0,0 +1,15 @@
+package com.solvd.api_tests.api;
+
+import com.zebrunner.carina.api.AbstractApiMethodV2;
+import com.zebrunner.carina.api.apitools.builder.NotStringValuesProcessor;
+import com.zebrunner.carina.utils.config.Configuration;
+
+public class GetRecipeById extends AbstractApiMethodV2 {
+    public GetRecipeById(int id) {
+        super(null, "api/recipe/get_recipe_rs.json");
+        replaceUrlPlaceholder("base_url", Configuration.getRequired("api_url"));
+        replaceUrlPlaceholder("id", String.valueOf(id));
+
+        ignorePropertiesProcessor(NotStringValuesProcessor.class);
+    }
+}
---
+++ src/test/java/com/solvd/api_tests/ProductTest.java
@@ -0,0 +1,105 @@
+package com.solvd.api_tests;
+
+import com.solvd.api_tests.api.DeleteProductById;
+import com.solvd.api_tests.api.GetProductById;
+import com.solvd.api_tests.api.PostProduct;
+import com.solvd.api_tests.api.PutProductById;
+import com.solvd.api_tests.domain.Product;
+import com.zebrunner.carina.api.apitools.validation.JsonComparatorContext;
+import com.zebrunner.carina.api.http.HttpResponseStatusType;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.time.DateTimeException;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.Objects;
+
+public class ProductTest {
+    @DataProvider(name = "validProductValues")
+    public Object[][] validProductValues() {
+        return new Object[][] {
+                {
+                    new Product(
+                            1,
+                            "iPhone 9",
+                            "An apple mobile which is nothing like apple",
+                            549,
+                            12.96,
+                            4.69,
+                            94,
+                            "Apple",
+                            "smartphones"
+                    )
+                }
+        };
+    }
+
+    @Test(description = "Verify getting product by id", dataProvider = "validProductValues")
+    public void verifyGetProductById(Product product) {
+        GetProductById getProductById = new GetProductById(product.getId());
+        getProductById.addProperty("product", product);
+
+        getProductById.expectResponseStatus(HttpResponseStatusType.OK_200);
+        getProductById.callAPI();
+
+        JsonComparatorContext comparatorContext = JsonComparatorContext.context()
+                .<Integer>withPredicate("idPredicate", id -> Objects.equals(id, product.getId()))
+                .<Integer>withPredicate("pricePredicate", price -> Objects.equals(price, product.getPrice()))
+                .<Double>withPredicate("discountPercentagePredicate", discountPercentage -> Objects.equals(discountPercentage, product.getDiscountPercentage()))
+                .<Double>withPredicate("ratingPredicate", rating -> Objects.equals(rating, product.getRating()))
+                .<Integer>withPredicate("stockPredicate",stock -> Objects.equals(stock, product.getStock()));
+
+        getProductById.validateResponse(comparatorContext);
+    }
+
+    @Test(description = "Verify creating product", dataProvider = "validProductValues")
+    public void verifyPostProduct(Product product) {
+        PostProduct postProduct = new PostProduct();
+        postProduct.addProperty("product", product);
+        postProduct.expectResponseStatus(HttpResponseStatusType.OK_200);
+        postProduct.callAPI();
+        postProduct.validateResponse();
+    }
+
+    @Test(description = "Verify deleting product by id", dataProvider = "validProductValues")
+    public void verifyDeleteProductById(Product product) {
+        DeleteProductById deleteProductById = new DeleteProductById(product.getId());
+        deleteProductById.addProperty("product", product);
+        deleteProductById.callAPIExpectSuccess();
+
+        JsonComparatorContext comparatorContext = JsonComparatorContext.context()
+                .<Integer>withPredicate("idPredicate", id -> Objects.equals(id, product.getId()))
+                .<Integer>withPredicate("pricePredicate", price -> Objects.equals(price, product.getPrice()))
+                .<Double>withPredicate("discountPercentagePredicate", discountPercentage -> Objects.equals(discountPercentage, product.getDiscountPercentage()))
+                .<Double>withPredicate("ratingPredicate", rating -> Objects.equals(rating, product.getRating()))
+                .<Integer>withPredicate("stockPredicate",stock -> Objects.equals(stock, product.getStock()))
+                .<String>withPredicate("datePredicate", date -> isDateValid(date) && ZonedDateTime.parse(date).isAfter(LocalDate.of(2024, 1, 1).atStartOfDay(ZoneId.systemDefault())))
+                .<Boolean>withPredicate("isDeletedPredicate", isDeleted -> isDeleted);
+
+        deleteProductById.validateResponse(comparatorContext);
+    }
+
+    private static boolean isDateValid(String date) {
+        try {
+            ZonedDateTime.parse(date);
+            return true;
+        } catch (DateTimeException e) {
+            return false;
+        }
+    }
+
+    @Test(description = "Verify updating product by id", dataProvider = "validProductValues")
+    public void verifyPutProductById(Product product) {
+        PutProductById putProductById = new PutProductById(product.getId());
+        product.setPrice(999);
+        putProductById.addProperty("product", product);
+        putProductById.callAPIExpectSuccess();
+
+        JsonComparatorContext comparatorContext = JsonComparatorContext.context()
+                .<Integer>withPredicate("idPredicate", id -> Objects.equals(id, product.getId()));
+
+        putProductById.validateResponse(comparatorContext);
+    }
+}
